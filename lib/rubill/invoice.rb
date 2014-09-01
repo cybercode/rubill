234198db4dbc6a3793c1bed4d17fce83598ec59c
Rick Frankel <rick@rickster.com>
2014-09-01 11:12:11 -0400

    update for prawn 1.2

--- lib/rubill/invoice.rb
+++ lib/rubill/invoice.rb
@@ -1,13 +1,13 @@
 require "prawn"
-require "prawn/layout"
+require "prawn/table"
 require "prawn/measurement_extensions"
 require 'rubill/calendar'
 require 'rubill/address_book'
 
 class Invoice
-  DEFAULT_ATTRS = { 
+  DEFAULT_ATTRS = {
     cell_style: {
-      border_width: 0, 
+      border_width: 0,
       border_color: 'ffffff',
       padding: 1
     },
@@ -18,7 +18,7 @@ class Invoice
     @calendar = Calendar.new(options[:calendar])
     @address_book = AddressBook.new
     @from = options[:from] || @calendar.last_billed_date+1
-    
+
     # default to end of of last month or today if today after
     # first week of month
     today = Date.today
@@ -36,7 +36,7 @@ class Invoice
     STDERR.puts "Generating invoice for #{@calendar.name} " +
       "from #{@from.to_s} to #{@to.to_s}"
   end
-  
+
   def save
     invoice_num = @calendar.next_invoice
     file = "#{@options[:directory]}/#{@calendar.name}_#{invoice_num}.pdf"
@@ -66,14 +66,14 @@ class Invoice
         borders: [:left],
         border_width: 2,
         padding: 1,
-      }, 
+      },
       column_widths: { 0 => 0.8.in, 2 => 0.5.in, 3 => 0.8.in }) do |t|
       t.row(-3..-1).font = 'Helvetica-Bold'
       t.rows(1..-1).columns(-2..-1).align = :right
       t.rows(1..-1).columns(-2).padding = [1, 3, 1, 1]
     end
     if logo=@options[:logo]
-      pdf.image logo['image'], at: [logo['x'], logo['y']], 
+      pdf.image logo['image'], at: [logo['x'], logo['y']],
       fit: [pdf.margin_box.width, logo['height']]
     end
     pdf.render_file(file)
@@ -93,19 +93,19 @@ class Invoice
   def line_items
     balance=@calendar.outstanding
     t = @calendar.items_for(@from, @to).collect { |e| e << e[2] * @rate }
-    
+
     t.sort! { |a,b| a[0] <=> b[0]}
     total = t.inject(0) { |s, r| s + r[3] }
     hours = t.inject(0) { |s, r| s + r[2]}
-    
-    t << 
+
+    t <<
       ['', 'Current',   hours, total]   <<
       ['', 'Balance',   '',    balance] <<
       ['', 'Total Due', '',    balance+total]
 
     t.each { |r| r[3] = fmt_ccy(r[3]) }
   end
-  
+
   def fmt_ccy v
     sprintf('%8.2f', v)
   end
